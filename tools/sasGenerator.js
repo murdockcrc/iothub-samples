9c313c06a1b264c90cbd874b2ba4a98cc43f2645
Luis Delgado <luis.delgado@fifa.org>
2022-09-24 20:54:52 +0200

    Adds SAS token generator and Postman collection

---
+++ tools/sasGenerator.js
@@ -0,0 +1,25 @@
+var crypto = require('crypto');
+
+var generateSasToken = function(resourceUri, signingKey, policyName, expiresInMins) {
+    resourceUri = encodeURIComponent(resourceUri);
+
+    // Set expiration in seconds
+    var expires = (Date.now() / 1000) + expiresInMins * 60;
+    expires = Math.ceil(expires);
+    var toSign = resourceUri + '\n' + expires;
+
+    // Use crypto
+    var hmac = crypto.createHmac('sha256', Buffer.from(signingKey, 'base64'));
+    hmac.update(toSign);
+    var base64UriEncoded = encodeURIComponent(hmac.digest('base64'));
+
+    // Construct authorization string
+    var token = "SharedAccessSignature sr=" + resourceUri + "&sig="
+    + base64UriEncoded + "&se=" + expires;
+    if (policyName) token += "&skn="+policyName;
+    return token;
+};
+
+var args = process.argv.slice(2);
+
+console.log(generateSasToken(args[0], args[1], args[2], args[3]))
